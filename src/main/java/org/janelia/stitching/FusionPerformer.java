c5036fdf0febcd72e9097978afd224c66b90658e
Igor Pisarev <pisarevi@janelia.hhmi.org>
2016-07-27 17:00:22 -0400

    Back to simple copy instead of fusion for now to speedup things

--- src/main/java/org/janelia/stitching/FusionPerformer.java
+++ src/main/java/org/janelia/stitching/FusionPerformer.java
@@ -5,15 +5,14 @@ import java.util.Arrays;
 
 import ij.IJ;
 import ij.ImagePlus;
-import mpicbg.stitching.fusion.AveragePixelFusion;
-import mpicbg.stitching.fusion.PixelFusion;
 import net.imglib2.Cursor;
 import net.imglib2.Point;
+import net.imglib2.RandomAccess;
 import net.imglib2.img.Img;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.img.imageplus.ImagePlusImgFactory;
 import net.imglib2.type.NativeType;
-import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.NumericType;
 import net.imglib2.view.IntervalView;
 import net.imglib2.view.Views;
 
@@ -33,7 +32,7 @@ public class FusionPerformer
 	}
 
 	@SuppressWarnings( "unchecked" )
-	public < T extends RealType< T > & NativeType< T > > void fuseTilesWithinSubregion( final ArrayList< TileInfo > tiles, final TileInfo subregion ) throws Exception
+	public < T extends NumericType< T > & NativeType< T > > void fuseTilesWithinSubregion( final ArrayList< TileInfo > tiles, final TileInfo subregion ) throws Exception
 	{
 		ImageType imageType = null;
 		for ( final TileInfo tile : tiles )
@@ -43,25 +42,20 @@ public class FusionPerformer
 			else if ( imageType != tile.getType() )
 				throw new Exception( "Can't fuse images of different types" );
 		}
-		
+
 		fuseSubregion( tiles, subregion, ( T ) imageType.getType() );
 	}
 
-	private < T extends RealType< T > & NativeType< T > > void fuseSubregion( final ArrayList< TileInfo > tiles, final TileInfo subregion, final T type ) throws Exception
+	private < T extends NumericType< T > & NativeType< T > > void fuseSubregion( final ArrayList< TileInfo > tiles, final TileInfo subregion, final T type ) throws Exception
 	{
 		final Boundaries subregionBoundaries = subregion.getBoundaries();
-		System.out.println( "subregion: " + Arrays.toString( subregionBoundaries.getDimensions() ) + ", tiles within subregion = " + tiles.size() );
-		
-		// TODO: improve flexibility by making this array n-dimensional
-		final PixelFusion[][][] pixels = new PixelFusion[ ( int ) subregionBoundaries.getDimensions( 0 ) ]
-		                                                [ ( int ) subregionBoundaries.getDimensions( 1 ) ]
-		                                                [ ( int ) subregionBoundaries.getDimensions( 2 ) ];
-		for ( int x = 0; x < pixels.length; x++ )
-			for ( int y = 0; y < pixels[ x ].length; y++ )
-				for ( int z = 0; z < pixels[ y ].length; z++ )
-					pixels[ x ][ y ][ z ] = new AveragePixelFusion(); // TODO: support different kinds of fusion
-		
-		// Fill the pixels
+
+		// Create output image
+		System.out.println( "subregion: " + Arrays.toString( subregionBoundaries.getDimensions() ) );
+		final Img< T > out = new ImagePlusImgFactory< T >().create( subregionBoundaries.getDimensions(), type.createVariable() );
+		final RandomAccess< T > randomAccess = out.randomAccess();
+
+		// Draw all intervals onto it one by one
 		for ( int i = 0; i < tiles.size(); i++ )
 		{
 			final TileInfo tile = tiles.get( i );
@@ -89,7 +83,7 @@ public class FusionPerformer
 				tileSubregionOffset[ d ] = Math.max( 0, ( long ) Math.floor( tile.getPosition( d ) ) - subregionBoundaries.getMin( d ) );
 
 			final long[] tileImageDimensions = tileBoundariesWithinSubregion.getDimensions();
-			final Img< T > in = ImageJFunctions.wrapReal( img );
+			final Img< T > in = ImageJFunctions.wrap( img );
 			final IntervalView< T > interval = Views.offsetInterval( in, tileImageOffset, tileImageDimensions );
 
 			String debug = "Processing tile " + ( i + 1 ) + " of " + tiles.size() + " ";
@@ -97,39 +91,22 @@ public class FusionPerformer
 				debug += "(" + ( interval.min( d ) + tileSubregionOffset[ d ] ) + "," + ( interval.max( d ) + tileSubregionOffset[ d ] ) + "),";
 			System.out.println( debug );
 
-			// Iterate through every pixel in the desired interval
-			final Cursor< T > inputCursor = interval.localizingCursor();
-			while ( inputCursor.hasNext() )
+			// Copy input to output
+			final Cursor< T > cursorInput = interval.localizingCursor();
+			while ( cursorInput.hasNext() )
 			{
-				inputCursor.fwd();
-				
-				final double[] inputPosition = new double[ interval.numDimensions() ];
-				inputCursor.localize( inputPosition );
-				
-				final Point outPosition = new Point( inputCursor );
-				outPosition.move( tileSubregionOffset );
-				
-				pixels[ outPosition.getIntPosition( 0 ) ]
-					  [ outPosition.getIntPosition( 1 ) ]
-					  [ outPosition.getIntPosition( 2 ) ].addValue( inputCursor.get().getRealDouble(), tile.getIndex(), inputPosition );
+				cursorInput.fwd();
+
+				final Point p = new Point( cursorInput );
+				p.move( tileSubregionOffset );
+				randomAccess.setPosition( p );
+
+				randomAccess.get().set( cursorInput.get() );
 			}
 			
 			img.close();
 		}
 
-		System.out.println( "Creating output image and filling it with data.." );
-		
-		final Img< T > out = new ImagePlusImgFactory< T >().create( subregionBoundaries.getDimensions(), type.createVariable() );
-		final Cursor< T > outCursor = out.localizingCursor();
-		while ( outCursor.hasNext() )
-		{
-			outCursor.fwd();
-			final Point outPosition = new Point( outCursor );
-			outCursor.get().setReal( pixels[ outPosition.getIntPosition( 0 ) ]
-										   [ outPosition.getIntPosition( 1 ) ]
-										   [ outPosition.getIntPosition( 2 ) ].getValue() );
-		}
-		
 		final ImagePlus outImg = ImageJFunctions.wrap( out, "" );
 		System.out.println( "Saving the resulting file for subregion " + subregion.getIndex() );
 		IJ.saveAsTiff( outImg, subregion.getFile() );
