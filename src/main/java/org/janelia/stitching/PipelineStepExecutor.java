f32f2ab7e1a9d96d7e1489caa308ea41366934a4
Igor Pisarev <pisarevi@janelia.hhmi.org>
2016-08-31 10:48:57 -0400

    Fix spark jobs serialization

--- src/main/java/org/janelia/stitching/PipelineStepExecutor.java
+++ src/main/java/org/janelia/stitching/PipelineStepExecutor.java
@@ -15,7 +15,7 @@ public abstract class PipelineStepExecutor implements Runnable, Serializable
 	private static final long serialVersionUID = 3546355803511705943L;
 
 	protected final StitchingJob job;
-	protected final JavaSparkContext sparkContext;
+	protected final transient JavaSparkContext sparkContext;
 
 	public PipelineStepExecutor( final StitchingJob job, final JavaSparkContext sparkContext )
 	{
