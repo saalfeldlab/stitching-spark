1e3d9971184d7bce198c3c2e04d92236e46b4cad
Igor Pisarev <pisarevi@janelia.hhmi.org>
2016-10-06 10:23:48 -0400

    Add missing file

---
+++ src/main/java/org/janelia/stitching/analysis/EstimateErrors.java
@@ -0,0 +1,174 @@
+package org.janelia.stitching.analysis;
+
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.janelia.stitching.TileInfo;
+import org.janelia.stitching.TileInfoJSONProvider;
+import org.janelia.stitching.TilePair;
+import org.janelia.stitching.Utils;
+
+public class EstimateErrors
+{
+	public static void main( final String[] args ) throws Exception
+	{
+		final Map< Integer, TileInfo > originalTiles = Utils.createTilesMap( TileInfoJSONProvider.loadTilesConfiguration( args[ 0 ] ) );
+		final TileInfo[] resultingTiles = TileInfoJSONProvider.loadTilesConfiguration( args[ 1 ] );
+
+		parseCoordinates( resultingTiles );
+		parseTimestamps( resultingTiles );
+		Arrays.sort( resultingTiles, new TileTimestampComparator() );
+
+		// subtract the first timestamp so we think the imaging starts at t=0
+		/*final long timeStart = timestamps.get( resultingTiles[ 0 ].getIndex() );
+		for ( final TileInfo tile : resultingTiles )
+			timestamps.put( tile.getIndex(), timestamps.get( tile.getIndex() ) - timeStart );*/
+
+		printConsecutivePairs( originalTiles, resultingTiles );
+		//printAllPairs( originalTiles, resultingTiles );
+	}
+
+
+	static void printAllPairs( final Map< Integer, TileInfo > originalTiles, final TileInfo[] resultingTiles ) throws Exception
+	{
+		final PrintWriter writer = new PrintWriter("/groups/saalfeld/saalfeldlab/betzigcollab/plots-20160923/Sample9/ch1-errors-allpairs.txt", "UTF-8");
+
+		int processed = 0;
+		System.out.println( "Total number of tiles: " + resultingTiles.length );
+		for ( int i = 0; i < resultingTiles.length; i++ )
+		{
+			for ( int j = i + 1; j < resultingTiles.length; j++ )
+			{
+				final TilePair pair = new TilePair( resultingTiles[ i ], resultingTiles[ j ] );
+
+				String s = "";
+				s += originalTiles.get( pair.first().getIndex() ).getPosition( 0 ) + " " + originalTiles.get( pair.first().getIndex() ).getPosition( 1 ) + " " + originalTiles.get( pair.first().getIndex() ).getPosition( 2 ) + " ";
+				s += originalTiles.get( pair.second().getIndex() ).getPosition( 0 ) + " " + originalTiles.get( pair.second().getIndex() ).getPosition( 1 ) + " " + originalTiles.get( pair.second().getIndex() ).getPosition( 2 ) + " ";
+
+				s += pair.first().getPosition( 0 ) + " " + pair.first().getPosition( 1 ) + " " + pair.first().getPosition( 2 ) + " ";
+				s += pair.second().getPosition( 0 ) + " " + pair.second().getPosition( 1 ) + " " + pair.second().getPosition( 2 ) + " ";
+
+				s += timestamps.get( pair.first().getIndex() ) + " " + timestamps.get( pair.second().getIndex() );
+
+				processed++;
+				writer.println( s );
+			}
+		}
+		System.out.println( "Processed: " + processed );
+		writer.close();
+	}
+
+
+
+	static void printConsecutivePairs( final Map< Integer, TileInfo > originalTiles, final TileInfo[] resultingTiles ) throws Exception
+	{
+		final List< TilePair > consecutivePairs = new ArrayList<>();
+		TileInfo prevTile = null;
+		for ( final TileInfo tile : resultingTiles )
+		{
+			if ( prevTile != null && timestamps.get( tile.getIndex() ) - timestamps.get( prevTile.getIndex() ) <= consecutiveDeltaTime )
+				consecutivePairs.add( new TilePair( prevTile, tile ) );
+			prevTile = tile;
+		}
+
+		System.out.println( consecutivePairs.size() + " consecutive pairs out of " + resultingTiles.length + " tiles total" );
+
+		final PrintWriter forwardsWriter = new PrintWriter("/groups/saalfeld/saalfeldlab/betzigcollab/plots-20160923/Sample9/ch1-errors-z-forwards.txt", "UTF-8");
+		final PrintWriter backwardsWriter = new PrintWriter("/groups/saalfeld/saalfeldlab/betzigcollab/plots-20160923/Sample9/ch1-errors-z-backwards.txt", "UTF-8");
+
+		int processed = 0;
+		for ( final TilePair pair : consecutivePairs )
+		{
+			if ( coordinates.get( pair.first().getIndex() ).x != coordinates.get( pair.second().getIndex() ).x || coordinates.get( pair.first().getIndex() ).y != coordinates.get( pair.second().getIndex() ).y )
+				continue;
+
+			String s = "";
+			s += originalTiles.get( pair.first().getIndex() ).getPosition( 0 ) + " " + originalTiles.get( pair.first().getIndex() ).getPosition( 1 ) + " " + originalTiles.get( pair.first().getIndex() ).getPosition( 2 ) + " ";
+			s += originalTiles.get( pair.second().getIndex() ).getPosition( 0 ) + " " + originalTiles.get( pair.second().getIndex() ).getPosition( 1 ) + " " + originalTiles.get( pair.second().getIndex() ).getPosition( 2 ) + " ";
+
+			s += pair.first().getPosition( 0 ) + " " + pair.first().getPosition( 1 ) + " " + pair.first().getPosition( 2 ) + " ";
+			s += pair.second().getPosition( 0 ) + " " + pair.second().getPosition( 1 ) + " " + pair.second().getPosition( 2 ) + " ";
+
+			processed++;
+			if ( coordinates.get( pair.first().getIndex() ).z < coordinates.get( pair.second().getIndex() ).z )
+				forwardsWriter.println( s );
+			else
+				backwardsWriter.println( s );
+		}
+		System.out.println( "Processed: " + processed );
+		forwardsWriter.close();
+		backwardsWriter.close();
+	}
+
+
+
+
+	static Map< Integer, Coordinates > coordinates;
+	static void parseCoordinates( final TileInfo[] tiles ) throws Exception
+	{
+		coordinates = new HashMap<>();
+		final String coordsPatternStr = ".*(\\d{3})x_(\\d{3})y_(\\d{3})z.*";
+		final Pattern coordsPattern = Pattern.compile( coordsPatternStr );
+		for ( final TileInfo tile : tiles )
+		{
+			final String filename = Paths.get( tile.getFilePath() ).getFileName().toString();
+			final Matcher matcher = coordsPattern.matcher( filename );
+			if ( !matcher.find() )
+				throw new Exception( "Can't parse coordinates" );
+
+			// swap X and Y axes
+			coordinates.put( tile.getIndex(), new Coordinates(
+					Integer.parseInt( matcher.group( 2 ) ),
+					Integer.parseInt( matcher.group( 1 ) ),
+					Integer.parseInt( matcher.group( 3 ) )
+					) );
+		}
+	}
+	static class Coordinates
+	{
+		public final int x,y,z;
+		public Coordinates( final int x, final int y, final int z )
+		{
+			this.x = x;
+			this.y = y;
+			this.z = z;
+		}
+	}
+
+
+	static final long consecutiveDeltaTime = 12000;
+	static Map< Integer, Long > timestamps;
+	static void parseTimestamps( final TileInfo[] tiles ) throws Exception
+	{
+		timestamps = new HashMap<>();
+		final String timePatternStr = ".*_(\\d*)msecAbs.*";
+		final Pattern timePattern = Pattern.compile( timePatternStr );
+		for ( final TileInfo tile : tiles )
+		{
+			final String filename = Paths.get( tile.getFilePath() ).getFileName().toString();
+			final Matcher matcher = timePattern.matcher( filename );
+			if ( !matcher.find() )
+				throw new Exception( "Can't parse timestamp" );
+
+			final long timestamp = Long.parseLong( matcher.group( 1 ) );
+			timestamps.put( tile.getIndex(), timestamp );
+		}
+	}
+
+	static class TileTimestampComparator implements Comparator< TileInfo >
+	{
+		@Override
+		public int compare( final TileInfo t1, final TileInfo t2 )
+		{
+			return Long.compare( timestamps.get( t1.getIndex() ), timestamps.get( t2.getIndex() ) );
+		}
+	}
+}
