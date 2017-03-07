0b03e18ff0320b0b1d2c04e9ee09e69744e890ea
Igor Pisarev <pisarevi@janelia.hhmi.org>
2017-03-07 18:57:56 -0500

    Improve shifted downsampling test by generating all possible scales

--- src/test/java/org/janelia/flatfield/ShiftedDownsamplingTest.java
+++ src/test/java/org/janelia/flatfield/ShiftedDownsamplingTest.java
@@ -1,13 +1,16 @@
 package org.janelia.flatfield;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.janelia.stitching.Utils;
 
 import ij.IJ;
 import ij.ImageJ;
 import ij.ImagePlus;
 import net.imglib2.FinalInterval;
-import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.RealRandomAccessible;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.img.imageplus.ImagePlusImgs;
 import net.imglib2.realtransform.AffineGet;
@@ -20,10 +23,10 @@ import net.imglib2.view.Views;
 
 public class ShiftedDownsamplingTest
 {
+	@SuppressWarnings("unchecked")
 	public static < A extends AffineGet & AffineSet, T extends NativeType< T > & RealType< T > > void main( final String[] args )
 	{
 		final String filepath = args[ 0 ];
-		final int lowScale = Integer.parseInt( args[ 1 ] ), restoredScale = Integer.parseInt( args[ 2 ] );
 
 		final ImagePlus imp = IJ.openImage( filepath );
 		Utils.workaroundImagePlusNSlices( imp );
@@ -51,25 +54,30 @@ public class ShiftedDownsamplingTest
 			throw new IllegalArgumentException( "Input image has " + img.numDimensions() + " dimensions (only 2d and 3d images are supported)" );
 		}
 
-		final ShiftedDownsampling downsampler = new ShiftedDownsampling( img, downsamplingTransform );
-		try ( final ShiftedDownsampling.PixelsMapping pixelsMapping = downsampler.new PixelsMapping( lowScale ) )
+		final ShiftedDownsampling< A > downsampler = new ShiftedDownsampling<>( img, downsamplingTransform );
+
+		final int restoredScale = 0;
+		final List< RandomAccessibleInterval< T > > restoredImgs = new ArrayList<>();
+		try ( final ShiftedDownsampling< A >.PixelsMapping pixelsMappingRestored = downsampler.new PixelsMapping( restoredScale ) )
 		{
-			final RandomAccessibleInterval< T > downsampledImg = downsampler.downsampleSolutionComponent( img, pixelsMapping );
-
-			try ( final ShiftedDownsampling.PixelsMapping pixelsMappingRestored = downsampler.new PixelsMapping( restoredScale ) )
+			for ( int lowScale = 0; lowScale < downsampler.getNumScales(); ++lowScale )
 			{
-				final RandomAccessibleInterval< T > referenceImg = downsampler.downsampleSolutionComponent( img, pixelsMappingRestored );
-
-				final RandomAccessible< T > upsampledImg = downsampler.upsample( downsampledImg, restoredScale );
-				final RandomAccessibleInterval< T > restoredImg = Views.interval( upsampledImg, new FinalInterval( pixelsMappingRestored.getDimensions() ) );
-
-				final RandomAccessibleInterval< T > testStackImg = Views.stack( referenceImg, restoredImg );
-				final ImagePlus testStackImp = ImageJFunctions.wrap( testStackImg, "reference/restored" );
-				Utils.workaroundImagePlusNSlices( testStackImp );
-
-				new ImageJ();
-				testStackImp.show();
+				System.out.println( "Processing scale " + lowScale );
+				try ( final ShiftedDownsampling< A >.PixelsMapping pixelsMapping = downsampler.new PixelsMapping( lowScale ) )
+				{
+					final RandomAccessibleInterval< T > downsampledImg = downsampler.downsampleSolutionComponent( img, pixelsMapping );
+					final RealRandomAccessible< T > upsampledImg = downsampler.upsample( downsampledImg, restoredScale );
+					final RandomAccessibleInterval< T > restoredImg = Views.interval( Views.raster( upsampledImg ), new FinalInterval( pixelsMappingRestored.getDimensions() ) );
+					restoredImgs.add( restoredImg );
+				}
 			}
 		}
+
+		final RandomAccessibleInterval< T > restoredImgsStack = Views.stack( restoredImgs );
+		final ImagePlus restoredStackImp = ImageJFunctions.wrap( restoredImgsStack, "" );
+		Utils.workaroundImagePlusNSlices( restoredStackImp );
+
+		new ImageJ();
+		restoredStackImp.show();
 	}
 }
