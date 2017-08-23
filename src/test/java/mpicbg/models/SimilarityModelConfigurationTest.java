ab7862b41d2e3d268f14dd639fc2c921de8a6987
Igor Pisarev <pisarevi@janelia.hhmi.org>
2017-08-23 18:10:48 -0400

    better way of finding square tile sub-configuration

--- src/test/java/mpicbg/models/SimilarityModelConfigurationTest.java
+++ src/test/java/mpicbg/models/SimilarityModelConfigurationTest.java
@@ -3,11 +3,16 @@ package mpicbg.models;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
+import java.util.Set;
 import java.util.TreeMap;
+import java.util.TreeSet;
 
 import org.janelia.stitching.SerializablePairWiseStitchingResult;
+import org.janelia.stitching.TileInfo;
 import org.janelia.stitching.TileInfoJSONProvider;
+import org.janelia.stitching.Utils;
 
 public class SimilarityModelConfigurationTest
 {
@@ -16,6 +21,8 @@ public class SimilarityModelConfigurationTest
 	public static void main( final String[] args ) throws Exception
 	{
 		final List< SerializablePairWiseStitchingResult[] > shiftsMulti = TileInfoJSONProvider.loadPairwiseShiftsMulti( "/nrs/saalfeld/igor/MB_310C_run2/test-similarity-model/iter0/pairwise.json" );
+
+		final Map< Integer, TileInfo > tilesMap = Utils.createTilesMapMulti( shiftsMulti, false );
 
 		final TreeMap< Integer, TreeMap< Integer, SerializablePairWiseStitchingResult[] > > connectedTiles = new TreeMap<>();
 		for ( final SerializablePairWiseStitchingResult[] shiftMulti : shiftsMulti )
@@ -30,68 +37,103 @@ public class SimilarityModelConfigurationTest
 		}
 
 		final List< Integer > square = new ArrayList<>();
-		square.add( connectedTiles.firstKey() );
-		square.add( connectedTiles.firstEntry().getValue().firstKey() );
-		A: for ( final Integer firstTileNeighbor : connectedTiles.get( square.get( 0 ) ).keySet() )
+		A: for ( final Entry< Integer, TreeMap< Integer, SerializablePairWiseStitchingResult[] > > firstCornerTileEntry : connectedTiles.entrySet() )
 		{
-			if ( firstTileNeighbor.intValue() == square.get( 1 ).intValue() )
-				continue;
-
-			for ( final Integer lastTileNeighbor : connectedTiles.get( square.get( 1 ) ).keySet() )
+			for ( final Entry< Integer, TreeMap< Integer, SerializablePairWiseStitchingResult[] > > secondCornerTileEntry : connectedTiles.entrySet() )
 			{
-				if ( lastTileNeighbor.intValue() == square.get( 0 ).intValue() )
+				if ( firstCornerTileEntry.getKey().intValue() == secondCornerTileEntry.getKey().intValue() )
 					continue;
 
-				if ( connectedTiles.get( firstTileNeighbor ).containsKey( lastTileNeighbor ) )
+				final Set< Integer > commonNeighbors = new TreeSet<>( firstCornerTileEntry.getValue().keySet() );
+				commonNeighbors.retainAll( secondCornerTileEntry.getValue().keySet() );
+				commonNeighbors.removeAll( Arrays.asList( firstCornerTileEntry.getKey(), secondCornerTileEntry.getKey() ) );
+				if ( commonNeighbors.size() >= 2 )
 				{
-					square.add( firstTileNeighbor );
-					square.add( lastTileNeighbor );
+					final List< Integer > commonNeighborsList = new ArrayList<>( commonNeighbors );
+					square.addAll( Arrays.asList( firstCornerTileEntry.getKey(), commonNeighborsList.get( 0 ), secondCornerTileEntry.getKey(), commonNeighborsList.get( 1 ) ) );
 					break A;
 				}
 			}
 		}
+		if ( square.isEmpty() )
+			throw new Exception( "no square subconfiguration found" );
 
-		System.out.println( "Found square formed by tiles " + square );
+		for ( int i = 1; i < square.size(); ++i )
+			if ( square.get( i ) <= square.get( i - 1 ) )
+				throw new Exception( "inconsistent square subconfiguration: " + square );
+
+		System.out.println( "Found square formed by tiles " + square + ":" );
+		for ( final Integer tileIndex : square )
+			System.out.println( "  " + Utils.getTileCoordinatesString( tilesMap.get( tileIndex ) ) );
 		System.out.println();
 
 		final TreeMap< Integer, Tile< ? > > tiles = new TreeMap<>();
 		for ( final Integer index : square )
 		{
 			tiles.put( index, new Tile<>(
-					new InterpolatedAffineModel3D<>(
-							new SimilarityModel3D(),
-							new TranslationModel3D(),
-							REGULARIZER_TRANSLATION
-						)
+					new RigidModel3D()
+
+//					new InterpolatedAffineModel3D<>(
+//							new SimilarityModel3D(),
+//							new TranslationModel3D(),
+//							REGULARIZER_TRANSLATION
+//						)
 				) );
 		}
 
 		for ( int i = 0; i < square.size(); ++i )
 		{
-			for ( int j = i + 1; j < square.size(); ++j )
+			final int ind1 = square.get( i ), ind2 = square.get( ( i + 1 ) % square.size() );
+			if ( !connectedTiles.get( ind1 ).containsKey( ind2 ) )
+				throw new Exception( "inconsistent square configuration: tiles " + ind1 + " and " + ind2 + " are not connected" );
+
+			final Tile< ? > tile1 = tiles.get( ind1 ), tile2 = tiles.get( ind2 );
+			tile1.addConnectedTile( tile2 );
+			tile2.addConnectedTile( tile1 );
+
+			final List< PointMatch > matches = new ArrayList<>();
+			for ( final SerializablePairWiseStitchingResult shift : connectedTiles.get( ind1 ).get( ind2 ) )
+				matches.add( new PointMatch( shift.getPointPair().getA(), shift.getPointPair().getB(), shift.getCrossCorrelation() ) );
+
+			tile1.addMatches( matches );
+			tile2.addMatches( PointMatch.flip( matches ) );
+
+			// check if matches are close to the original shift between tiles
+			double[] stageOffset = null;
+			for ( final SerializablePairWiseStitchingResult shift : connectedTiles.get( ind1 ).get( ind2 ) )
 			{
-				if ( connectedTiles.get( square.get( i ) ).containsKey( square.get( j ) ) )
+				if ( stageOffset == null )
 				{
-					final int ind1 = square.get( i ), ind2 = square.get( j );
-					final Tile< ? > tile1 = tiles.get( ind1 ), tile2 = tiles.get( ind2 );
-					tile1.addConnectedTile( tile2 );
-					tile2.addConnectedTile( tile1 );
-
-					final List< PointMatch > matches = new ArrayList<>();
-					for ( final SerializablePairWiseStitchingResult shift : connectedTiles.get( ind1 ).get( ind2 ) )
-						matches.add( new PointMatch( shift.getPointPair().getA(), shift.getPointPair().getB(), shift.getCrossCorrelation() ) );
-
-					tile1.addMatches( matches );
-					tile2.addMatches( PointMatch.flip( matches ) );
+					stageOffset = new double[ shift.getNumDimensions() ];
+					for ( int d = 0; d < stageOffset.length; ++d )
+						stageOffset[ d ] = shift.getTilePair().getB().getPosition( d ) - shift.getTilePair().getA().getPosition( d );
+					System.out.println( shift.getTilePair() );//+ ": stageOffset=" + Arrays.toString( stageOffset ) );
 				}
+				else
+				{
+					for ( int d = 0; d < stageOffset.length; ++d )
+						if ( Math.abs( stageOffset[ d ] - shift.getTilePair().getB().getPosition( d ) - shift.getTilePair().getA().getPosition( d ) ) > 1e10 )
+							throw new RuntimeException( "inconsistent tile configuration" );
+				}
+
+				for ( int d = 0; d < stageOffset.length; ++d )
+					if ( Math.abs( shift.getOffset( d ) - ( shift.getPointPair().getA().getL()[ d ] - shift.getPointPair().getB().getL()[ d ] ) ) > 1e10 )
+						throw new RuntimeException( "point matches are inconsistent with estimated offset" );
+
+				final double[] diff = new double[ stageOffset.length ];
+				for ( int d = 0; d < stageOffset.length; ++d )
+					diff[ d ] = stageOffset[ d ] - shift.getOffset( d );
+				System.out.println( "  diff=" + Arrays.toString( diff ) );
 			}
+			System.out.println();
 		}
 
 		final TileConfiguration tc = new TileConfiguration();
 		tc.addTiles( tiles.values() );
-		tc.fixTile( tiles.firstEntry().getValue() );
 		tc.preAlign();
 		tc.optimize( 10, 1000, 1000, 1.f, 1 );
+
+		System.out.println();
 
 		for ( final Entry< Integer, Tile< ? > > entry : tiles.entrySet() )
 		{
