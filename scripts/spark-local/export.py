18c0413325a932bc616e4facde6cc582b287e4e5
Igor Pisarev <pisarevi@janelia.hhmi.org>
2017-09-17 21:00:33 -0400

    add startup scripts for local runs

---
+++ scripts/spark-local/export.py
@@ -0,0 +1,4 @@
+import sys
+import subprocess
+
+subprocess.call(['java', '-Dspark.master=local[*]' '-cp', '../../target/stitching-spark-0.0.1-SNAPSHOT.jar', 'org.janelia.stitching.StitchingSpark', '--fuse'] + sys.argv[1:])
