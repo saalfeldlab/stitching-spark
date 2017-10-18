11753c4e6d2a2b1ffb5737cc2907bc62bbc364c7
Igor Pisarev <pisarevi@janelia.hhmi.org>
2017-10-18 17:47:05 -0400

    support s3 url protocol
    based on https://github.com/saalfeldlab/render/pull/43

---
+++ src/main/java/org/janelia/dataaccess/CustomURLStreamHandlerFactory.java
@@ -0,0 +1,40 @@
+package org.janelia.dataaccess;
+
+import java.net.URL;
+import java.net.URLStreamHandler;
+import java.net.URLStreamHandlerFactory;
+
+import com.amazonaws.services.s3.AmazonS3;
+
+/**
+ * Factory for all supported custom protocols.
+ *
+ * The core method {@link URL#setURLStreamHandlerFactory(URLStreamHandlerFactory)}
+ * can be called at most once, therefore this class must handle all supported protocols.
+ *
+ * @author Igor Pisarev
+ */
+class CustomURLStreamHandlerFactory implements URLStreamHandlerFactory
+{
+	private static final String s3Protocol = "s3";
+
+	private final AmazonS3 s3;
+
+	private CustomURLStreamHandlerFactory( final AmazonS3 s3 )
+	{
+		this.s3 = s3;
+	}
+
+	public static void init( final AmazonS3 s3 )
+	{
+		URL.setURLStreamHandlerFactory( new CustomURLStreamHandlerFactory( s3 ) );
+	}
+
+	@Override
+	public URLStreamHandler createURLStreamHandler( final String protocol )
+	{
+		if ( protocol.equals( s3Protocol ) )
+			return new S3URLStreamHandler( s3 );
+		return null;
+	}
+}
---
+++ src/main/java/org/janelia/dataaccess/S3URLConnection.java
@@ -0,0 +1,44 @@
+package org.janelia.dataaccess;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3URI;
+import com.amazonaws.services.s3.model.S3Object;
+
+class S3URLConnection extends URLConnection
+{
+	private final AmazonS3 s3;
+	private S3Object s3Object;
+
+	public S3URLConnection( final AmazonS3 s3, final URL url )
+	{
+		super( url );
+		this.s3 = s3;
+	}
+
+	@Override
+	public void connect() throws IOException
+	{
+		final AmazonS3URI s3Uri = AmazonS3DataProvider.decodeS3Uri( url.toString() );
+		s3Object = s3.getObject( s3Uri.getBucket(), s3Uri.getKey() );
+		connected = true;
+	}
+
+	@Override
+	public String getContentType()
+	{
+		return s3Object.getObjectMetadata().getContentType();
+	}
+
+	@Override
+	public InputStream getInputStream() throws IOException
+	{
+		if ( !connected )
+			connect();
+		return s3Object.getObjectContent();
+	}
+}
