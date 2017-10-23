b1a4937c72868336ebc49044913036d72429cac9
Igor Pisarev <pisarevi@janelia.hhmi.org>
2017-10-23 17:57:23 -0400

    pass s3 client directly instead of builder

--- src/main/java/org/janelia/dataaccess/S3URLConnection.java
+++ src/main/java/org/janelia/dataaccess/S3URLConnection.java
@@ -2,6 +2,8 @@ package org.janelia.dataaccess;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
 
@@ -23,7 +25,16 @@ class S3URLConnection extends URLConnection
 	@Override
 	public void connect() throws IOException
 	{
-		final AmazonS3URI s3Uri = AmazonS3DataProvider.decodeS3Uri( url.toString() );
+		final URI uri;
+		try
+		{
+			uri = url.toURI();
+		}
+		catch ( final URISyntaxException e )
+		{
+			throw new RuntimeException( e );
+		}
+		final AmazonS3URI s3Uri = AmazonS3DataProvider.decodeS3Uri( uri );
 		s3Object = s3.getObject( s3Uri.getBucket(), s3Uri.getKey() );
 		connected = true;
 	}
--- src/main/java/org/janelia/dataaccess/S3URLStreamHandler.java
+++ src/main/java/org/janelia/dataaccess/S3URLStreamHandler.java
@@ -6,17 +6,14 @@ import java.net.URLConnection;
 import java.net.URLStreamHandler;
 
 import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
 class S3URLStreamHandler extends URLStreamHandler
 {
-	private final AmazonS3ClientBuilder s3Builder;
-	private final transient AmazonS3 s3;
+	private final AmazonS3 s3;
 
-	public S3URLStreamHandler( final AmazonS3ClientBuilder s3Builder )
+	public S3URLStreamHandler( final AmazonS3 s3 )
 	{
-		this.s3Builder = s3Builder;
-		s3 = s3Builder.build();
+		this.s3 = s3;
 	}
 
 	@Override
