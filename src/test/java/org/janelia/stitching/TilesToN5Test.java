4c14bd58c31d505a1f50c6d413abf326246de349
Igor Pisarev <pisarevi@janelia.hhmi.org>
2017-10-30 18:05:42 -0400

    add aws s3-based test for converting tiles to n5

--- src/test/java/org/janelia/stitching/TilesToN5Test.java
+++ src/test/java/org/janelia/stitching/TilesToN5Test.java
@@ -16,8 +16,11 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.janelia.saalfeldlab.n5.CompressionType;
 import org.janelia.saalfeldlab.n5.N5;
 import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
+import org.janelia.saalfeldlab.n5.s3.N5AmazonS3;
 import org.junit.Assert;
 import org.junit.Test;
+
+import com.amazonaws.services.s3.AmazonS3URI;
 
 import ij.IJ;
 import ij.ImagePlus;
@@ -34,7 +37,7 @@ import net.imglib2.view.Views;
 public class TilesToN5Test
 {
 	@Test
-	public void test() throws ImgLibException, IOException
+	public void testFS() throws ImgLibException, IOException
 	{
 		final Random rnd = new Random();
 		final long[] dimensions = new long[ 3 ];
@@ -95,6 +98,94 @@ public class TilesToN5Test
 		System.out.println( "Loading N5 cell export from:" + Paths.get( n5Path, datasetPath ).toString() );
 
 		final RandomAccessibleInterval< UnsignedShortType > rai = N5Utils.open( N5.openFSReader( n5Path ), Paths.get( datasetPath, impFilename ).toString() );
+		final Cursor< UnsignedShortType > raiCursor = Views.iterable( rai ).localizingCursor();
+		final RandomAccess< UnsignedShortType > imgRandomAccess = img.randomAccess();
+		int pixelsProcessed = 0;
+		while ( raiCursor.hasNext() )
+		{
+			++pixelsProcessed;
+			raiCursor.fwd();
+			imgRandomAccess.setPosition( raiCursor );
+			if ( !raiCursor.get().valueEquals( imgRandomAccess.get() ) )
+			{
+				final long[] position = new long[ raiCursor.numDimensions() ];
+				raiCursor.localize( position );
+				fail( "pixel value differs at " + Arrays.toString( position ) );
+			}
+		}
+
+		if ( pixelsProcessed != Intervals.numElements( dimensions ) )
+			fail( "processed less pixels than contained in the image" );
+
+		System.out.println( "All checks passed, removing temp data..." );
+		N5.openFSWriter( tempDir.toString() ).remove();
+
+		System.out.println( "OK" );
+	}
+
+//	@Test
+	public void testS3() throws ImgLibException, IOException
+	{
+		final Random rnd = new Random();
+		final long[] dimensions = new long[ 3 ];
+		for ( int d = 0; d < dimensions.length; ++d )
+			dimensions[ d ] = rnd.nextInt( 700 ) + 1;
+
+		final int blockSize = rnd.nextInt( 100 ) + 30;
+
+		System.out.println( "Generating random image of size " + Arrays.toString( dimensions ) + ", block size set to " + blockSize );
+
+		final ImagePlusImg< UnsignedShortType, ? > img = ImagePlusImgs.unsignedShorts( dimensions );
+		final Cursor< UnsignedShortType > cursor = img.cursor();
+		while ( cursor.hasNext() )
+			cursor.next().set( ( int ) Math.round( ( rnd.nextGaussian() + 1.0 ) * 100 ) );
+
+		final ImagePlus imp = img.getImagePlus();
+		Utils.workaroundImagePlusNSlices( imp );
+
+		final Path tempDir = Files.createTempDirectory( "TilesToN5Test-" );
+		final String impFilename = "test-imp.tif";
+		final String impPath = tempDir.resolve( impFilename ).toString();
+
+		System.out.println( "Saving test image to: " + impPath );
+		IJ.saveAsTiff( imp, impPath );
+
+		final TileInfo tile = new TileInfo( dimensions.length );
+		tile.setIndex( 0 );
+		tile.setSize( dimensions );
+		tile.setPosition( new double[ dimensions.length ] );
+		tile.setFilePath( impPath );
+		tile.setType( ImageType.GRAY16 );
+		final TileInfo[] tiles = new TileInfo[] { tile };
+
+		final String n5Bucket = "test-bucket-n5";
+		final String s3Link = new AmazonS3URI( "s3://" + n5Bucket + "/" ).toString();
+		final String datasetPath = "test-channel";
+
+		try ( final JavaSparkContext sparkContext = new JavaSparkContext( new SparkConf()
+				.setMaster( "local" )
+				.setAppName( "TilesToN5Test" )
+				.set( "spark.serializer", "org.apache.spark.serializer.KryoSerializer" )
+			) )
+		{
+			final Map< String, TileInfo[] > newTiles = TilesToN5Converter.convertTiffToN5(
+					sparkContext,
+					s3Link,
+					() -> N5AmazonS3.openS3Writer( n5Bucket ),
+					Collections.singletonMap( datasetPath, tiles ),
+					blockSize,
+					CompressionType.GZIP
+				);
+
+			Assert.assertEquals( datasetPath, newTiles.keySet().iterator().next() );
+
+			final TileInfo newTile = newTiles.values().iterator().next()[ 0 ];
+			Assert.assertEquals( Paths.get( s3Link, datasetPath, impFilename ).toString(), newTile.getFilePath() );
+		}
+
+		System.out.println( "Loading N5 cell export from:" + Paths.get( s3Link, datasetPath ).toString() );
+
+		final RandomAccessibleInterval< UnsignedShortType > rai = N5Utils.open( N5AmazonS3.openS3Reader( n5Bucket ), Paths.get( datasetPath, impFilename ).toString() );
 		final Cursor< UnsignedShortType > raiCursor = Views.iterable( rai ).localizingCursor();
 		final RandomAccess< UnsignedShortType > imgRandomAccess = img.randomAccess();
 		int pixelsProcessed = 0;
