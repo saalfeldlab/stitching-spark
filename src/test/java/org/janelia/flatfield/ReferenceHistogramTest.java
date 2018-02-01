39d5124d2e7bb9d17cf4d0f3297f0af8ff56b0bf
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-02-01 17:28:44 -0500

    update reference histogram test

--- src/test/java/org/janelia/flatfield/ReferenceHistogramTest.java
+++ src/test/java/org/janelia/flatfield/ReferenceHistogramTest.java
@@ -9,18 +9,18 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.janelia.dataaccess.DataProvider;
 import org.janelia.dataaccess.DataProviderFactory;
-import org.janelia.histogram.Histogram;
-import org.janelia.saalfeldlab.n5.DataType;
 import org.janelia.saalfeldlab.n5.GzipCompression;
 import org.janelia.saalfeldlab.n5.N5Writer;
 import org.janelia.saalfeldlab.n5.bdv.DataAccessType;
+import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import net.imglib2.img.list.ListLocalizingCursor;
-import net.imglib2.img.list.WrappedListImg;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.type.numeric.real.DoubleType;
 
 public class ReferenceHistogramTest
 {
@@ -54,125 +54,136 @@ public class ReferenceHistogramTest
 	}
 
 
+	/**
+	 * Test reference histogram where
+	 * the number of histograms is even, and
+	 * the number of filtered histograms is even.
+	 * @throws IOException
+	 */
 	@Test
 	public void testEvenEven() throws IOException
 	{
-		final List< Histogram > histograms = new ArrayList<>();
-		histograms.add( createHistogram( new int[] { 1, 4, 2, 0, 1 } ) );
-		histograms.add( createHistogram( new int[] { 2, 2, 2, 2, 0 } ) );
-		histograms.add( createHistogram( new int[] { 3, 1, 0, 1, 3 } ) );
-		histograms.add( createHistogram( new int[] { 2, 5, 1, 0, 0 } ) );
+		final List< double[] > histograms = new ArrayList<>();
+		histograms.add( addTailBins( 1, 4, 2, 0, 1 ) );
+		histograms.add( addTailBins( 2, 2, 2, 2, 0 ) );
+		histograms.add( addTailBins( 3, 1, 0, 1, 3 ) );
+		histograms.add( addTailBins( 2, 5, 1, 0, 0 ) );
 
-		final Histogram accumulatedHistogram = getReferenceHistogram( histograms );
-		Assert.assertArrayEquals( new double[] { 1.5, 3, 2, 1, 0.5 }, getHistogramArray( accumulatedHistogram ), EPSILON );
+		final double[] accumulatedHistogram = getReferenceHistogram( histograms );
+		Assert.assertArrayEquals( addTailBins( 1.5, 3, 2, 1, 0.5 ), accumulatedHistogram, EPSILON );
 	}
 
+	/**
+	 * Test reference histogram where
+	 * the number of histograms is even, and
+	 * the number of filtered histograms is odd.
+	 * @throws IOException
+	 */
 	@Test
 	public void testEvenOdd() throws IOException
 	{
-		final List< Histogram > histograms = new ArrayList<>();
-		histograms.add( createHistogram( new int[] { 1, 4, 2, 0, 1 } ) );
-		histograms.add( createHistogram( new int[] { 2, 2, 2, 2, 0 } ) );
-		histograms.add( createHistogram( new int[] { 3, 1, 0, 1, 3 } ) );
-		histograms.add( createHistogram( new int[] { 2, 5, 1, 0, 0 } ) );
-		histograms.add( createHistogram( new int[] { 1, 3, 3, 1, 0 } ) );
-		histograms.add( createHistogram( new int[] { 0, 1, 6, 1, 0 } ) );
+		final List< double[] > histograms = new ArrayList<>();
+		histograms.add( addTailBins( 1, 4, 2, 0, 1 ) );
+		histograms.add( addTailBins( 2, 2, 2, 2, 0 ) );
+		histograms.add( addTailBins( 3, 1, 0, 1, 3 ) );
+		histograms.add( addTailBins( 2, 5, 1, 0, 0 ) );
+		histograms.add( addTailBins( 1, 3, 3, 1, 0 ) );
+		histograms.add( addTailBins( 0, 1, 6, 1, 0 ) );
 
-		final Histogram accumulatedHistogram = getReferenceHistogram( histograms );
-		Assert.assertArrayEquals( new double[] { 1.3333333333, 3, 2.3333333333, 1, 0.3333333333 }, getHistogramArray( accumulatedHistogram ), EPSILON );
+		final double[] accumulatedHistogram = getReferenceHistogram( histograms );
+		Assert.assertArrayEquals( addTailBins( 1.3333333333, 3, 2.3333333333, 1, 0.3333333333 ), accumulatedHistogram, EPSILON );
 	}
 
+	/**
+	 * Test reference histogram where
+	 * the number of histograms is odd, and
+	 * the number of filtered histograms is even.
+	 * @throws IOException
+	 */
 	@Test
 	public void testOddEven() throws IOException
 	{
-		final List< Histogram > histograms = new ArrayList<>();
-		histograms.add( createHistogram( new int[] { 1, 4, 2, 0, 1 } ) );
-		histograms.add( createHistogram( new int[] { 2, 2, 2, 2, 0 } ) );
-		histograms.add( createHistogram( new int[] { 3, 1, 0, 1, 3 } ) );
-		histograms.add( createHistogram( new int[] { 2, 5, 1, 0, 0 } ) );
-		histograms.add( createHistogram( new int[] { 1, 3, 3, 1, 0 } ) );
-		histograms.add( createHistogram( new int[] { 0, 1, 6, 1, 0 } ) );
-		histograms.add( createHistogram( new int[] { 1, 2, 1, 2, 2 } ) );
+		final List< double[] > histograms = new ArrayList<>();
+		histograms.add( addTailBins( 1, 4, 2, 0, 1 ) );
+		histograms.add( addTailBins( 2, 2, 2, 2, 0 ) );
+		histograms.add( addTailBins( 3, 1, 0, 1, 3 ) );
+		histograms.add( addTailBins( 2, 5, 1, 0, 0 ) );
+		histograms.add( addTailBins( 1, 3, 3, 1, 0 ) );
+		histograms.add( addTailBins( 0, 1, 6, 1, 0 ) );
+		histograms.add( addTailBins( 1, 2, 1, 2, 2 ) );
 
-		final Histogram accumulatedHistogram = getReferenceHistogram( histograms );
-		Assert.assertArrayEquals( new double[] { 1.5, 1.75, 2.75, 1.25, 0.75 }, getHistogramArray( accumulatedHistogram ), EPSILON );
+		final double[] accumulatedHistogram = getReferenceHistogram( histograms );
+		Assert.assertArrayEquals( addTailBins( 1.5, 1.75, 2.75, 1.25, 0.75 ), accumulatedHistogram, EPSILON );
 	}
 
+	/**
+	 * Test reference histogram where
+	 * the number of histograms is odd, and
+	 * the number of filtered histograms is odd.
+	 * @throws IOException
+	 */
 	@Test
 	public void testOddOdd() throws IOException
 	{
-		final List< Histogram > histograms = new ArrayList<>();
-		histograms.add( createHistogram( new int[] { 1, 4, 2, 0, 1 } ) );
-		histograms.add( createHistogram( new int[] { 2, 2, 2, 2, 0 } ) );
-		histograms.add( createHistogram( new int[] { 3, 1, 0, 1, 3 } ) );
-		histograms.add( createHistogram( new int[] { 2, 5, 1, 0, 0 } ) );
-		histograms.add( createHistogram( new int[] { 0, 1, 6, 1, 0 } ) );
+		final List< double[] > histograms = new ArrayList<>();
+		histograms.add( addTailBins( 1, 4, 2, 0, 1 ) );
+		histograms.add( addTailBins( 2, 2, 2, 2, 0 ) );
+		histograms.add( addTailBins( 3, 1, 0, 1, 3 ) );
+		histograms.add( addTailBins( 2, 5, 1, 0, 0 ) );
+		histograms.add( addTailBins( 0, 1, 6, 1, 0 ) );
 
-		final Histogram accumulatedHistogram = getReferenceHistogram( histograms );
-		Assert.assertArrayEquals( new double[] { 2, 2.3333333333, 1.3333333333, 1, 1.3333333333 }, getHistogramArray( accumulatedHistogram ), EPSILON );
+		final double[] accumulatedHistogram = getReferenceHistogram( histograms );
+		Assert.assertArrayEquals( addTailBins( 2, 2.3333333333, 1.3333333333, 1, 1.3333333333 ), accumulatedHistogram, EPSILON );
 	}
 
-	private Histogram createHistogram( final int... binElements )
+	private double[] getReferenceHistogram( final List< double[] > histograms ) throws IOException
 	{
-		final Histogram histogram = new Histogram( histMinValue, histMaxValue, binElements.length );
-		for ( int i = 0; i < binElements.length; ++i )
-			histogram.put( histogram.getBinValue( i ), binElements[ i ] );
-		return histogram;
-	}
+		final int bins = histograms.get( 0 ).length;
+		for ( final double[] histogram : histograms )
+			assert histogram.length == bins;
 
-	private Histogram getReferenceHistogram( final List< Histogram > histograms ) throws IOException
-	{
 		final DataProvider dataProvider = DataProviderFactory.createFSDataProvider();
 		final DataAccessType dataAccessType = DataAccessType.FILESYSTEM;
 
 		final long[] dimensions = new long[] { 1, 1, histograms.size() };
 		final int[] blockSize = new int[] { 1, 1, histograms.size() / 2 };
 
+		final long[] extendedDimensions = new long[ dimensions.length + 1 ];
+		System.arraycopy( dimensions, 0, extendedDimensions, 0, dimensions.length );
+		extendedDimensions[ dimensions.length ] = bins;
+
+		final int[] extendedBlockSize = new int[ blockSize.length + 1 ];
+		System.arraycopy( blockSize, 0, extendedBlockSize, 0, blockSize.length );
+		extendedBlockSize[ blockSize.length ] = bins;
+
 		// save histograms first
+		final double[] histogramsHelperArray = new double[ histograms.size() * bins ];
+		int helperArrayIndex = 0;
+		for ( int bin = 0; bin < bins; ++bin )
+			for ( int i = 0; i < histograms.size(); ++i )
+				histogramsHelperArray[ helperArrayIndex++ ] = histograms.get( i )[ bin ];
+
 		final N5Writer n5 = dataProvider.createN5Writer( URI.create( histogramsN5BasePath ) );
-		n5.createDataset(
-				histogramsDataset,
-				dimensions,
-				blockSize,
-				DataType.SERIALIZABLE,
-				new GzipCompression()
-			);
-		for ( int i = 0; i < dimensions[ dimensions.length - 1 ]; i += blockSize[ blockSize.length - 1 ] )
-		{
-			final long[] blockPosition = new long[ blockSize.length ];
-			blockPosition[ blockPosition.length - 1 ] = i / blockSize[ blockSize.length - 1 ];
-			final WrappedSerializableDataBlockWriter< Histogram > histogramsBlock = new WrappedSerializableDataBlockWriter<>( n5, histogramsDataset, blockPosition );
-			Assert.assertFalse( histogramsBlock.wasLoadedSuccessfully() );
+		final RandomAccessibleInterval< DoubleType > source = ArrayImgs.doubles( histogramsHelperArray, extendedDimensions );
+		N5Utils.save( source, n5, histogramsDataset, extendedBlockSize, new GzipCompression() );
 
-			final WrappedListImg< Histogram > histogramsBlockImg = histogramsBlock.wrap();
-			final ListLocalizingCursor< Histogram > histogramsBlockImgCursor = histogramsBlockImg.localizingCursor();
-			while ( histogramsBlockImgCursor.hasNext() )
-			{
-				histogramsBlockImgCursor.fwd();
-				final int pos = histogramsBlockImgCursor.getIntPosition( histogramsBlockImg.numDimensions() - 1 );
-				histogramsBlockImgCursor.set( histograms.get( i + pos ) );
-			}
-			histogramsBlock.save();
-		}
-
-		final Histogram referenceHistogram = HistogramsProvider.estimateReferenceHistogram(
+		final double[] referenceHistogram = HistogramsProvider.estimateReferenceHistogram(
 				sparkContext,
 				dataProvider, dataAccessType,
 				histogramsN5BasePath, histogramsDataset,
 				dimensions, blockSize,
 				REFERENCE_HISTOGRAM_POINTS_PERCENT,
-				histMinValue, histMaxValue, histograms.get( 0 ).getNumBins()
+				histMinValue, histMaxValue, bins
 			);
 
 		Assert.assertTrue( n5.remove() );
 		return referenceHistogram;
 	}
 
-	private double[] getHistogramArray( final Histogram histogram )
+	private double[] addTailBins( final double... histogram )
 	{
-		final double[] array = new double[ histogram.getNumBins() ];
-		for ( int i = 0; i < array.length; ++i )
-			array[ i ] = histogram.get( i );
-		return array;
+		final double[] histogramWithTailBins = new double[ histogram.length + 2 ];
+		System.arraycopy( histogram, 0, histogramWithTailBins, 1, histogram.length );
+		return histogramWithTailBins;
 	}
 }
