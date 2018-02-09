eb5502efcd844a009d355e37a9d2e76c8d2cc637
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-02-09 18:36:08 -0500

    use max number of cores for flatfield estimation driver app

--- startup-scripts/spark-janelia/flatfield.py
+++ startup-scripts/spark-janelia/flatfield.py
@@ -12,7 +12,7 @@ flintstone_file = os.path.join('flintstone', 'flintstone.sh')
 flintstone_path = os.path.join(curr_script_dir, flintstone_file)
 
 os.environ['SPARK_VERSION'] = 'test'
-os.environ['N_DRIVER_THREADS'] = '2'
+os.environ['N_DRIVER_THREADS'] = '16'
 os.environ['MEMORY_PER_NODE'] = '105'
 os.environ['TERMINATE'] = '1'
 
