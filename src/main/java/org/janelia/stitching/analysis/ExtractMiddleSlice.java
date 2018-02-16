3d0562248acecddee8560895072156270146ac6b
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-02-16 18:19:16 -0500

    add output folder argument to middle slice extractor script

--- src/main/java/org/janelia/stitching/analysis/ExtractMiddleSlice.java
+++ src/main/java/org/janelia/stitching/analysis/ExtractMiddleSlice.java
@@ -1,7 +1,6 @@
 package org.janelia.stitching.analysis;
 
 import java.io.File;
-import java.io.Serializable;
 import java.net.URI;
 import java.nio.file.Paths;
 import java.util.Arrays;
@@ -29,56 +28,50 @@ import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.view.Views;
 
-public class ExtractMiddleSlice implements Serializable {
-
-	private static final long serialVersionUID = -5089082519880731804L;
-
-	private transient final JavaSparkContext sparkContext;
-	private final String outFolder;
-
+public class ExtractMiddleSlice
+{
 	public static void main( final String[] args ) throws Exception
 	{
-		new ExtractMiddleSlice( args[ 0 ] );
-	}
+		final String input = args[ 0 ];
+		final String outFolder = args.length > 1 ? args[ 1 ] : Paths.get( Paths.get( input ).getParent().toString(), "middle" ).toString();
+		new File( outFolder ).mkdirs();
 
-	public ExtractMiddleSlice( final String input ) throws Exception
-	{
 		final TileInfo[] tiles = TileInfoJSONProvider.loadTilesConfiguration( DataProviderFactory.createFSDataProvider().getJsonReader( URI.create( input ) ) );
+		final TileInfo[] middleSliceTiles;
 
-		sparkContext = new JavaSparkContext( new SparkConf().setAppName( "ExtractMiddleSlice" ));
-		outFolder = Paths.get(input).getParent().toString()+"/middle";
-		new File(outFolder).mkdirs();
-
-		final JavaRDD< TileInfo > rdd = sparkContext.parallelize( Arrays.asList( tiles ) );
-		final JavaRDD< TileInfo > task = rdd.map(
-				new Function< TileInfo, TileInfo >()
-				{
-					private static final long serialVersionUID = 4991255417353136684L;
-
-					@Override
-					public TileInfo call( final TileInfo tile ) throws Exception
+		try ( final JavaSparkContext sparkContext = new JavaSparkContext( new SparkConf().setAppName( "ExtractMiddleSlice" ) ) )
+		{
+			final JavaRDD< TileInfo > rdd = sparkContext.parallelize( Arrays.asList( tiles ) );
+			final JavaRDD< TileInfo > task = rdd.map(
+					new Function< TileInfo, TileInfo >()
 					{
-						final String outPath = outFolder+"/MID_"+Paths.get(tile.getFilePath()).getFileName().toString();
-						extractMiddleSlice( tile, outPath );
+						private static final long serialVersionUID = 4991255417353136684L;
 
-						final TileInfo middleSliceTile = new TileInfo(tile.numDimensions() - 1);
-						middleSliceTile.setIndex( tile.getIndex() );
-						middleSliceTile.setType( tile.getType() );
-						middleSliceTile.setPosition( new double[] { tile.getPosition(0), tile.getPosition(1) } );
-						middleSliceTile.setSize( new long[] { tile.getSize(0), tile.getSize(1) } );
-						middleSliceTile.setFilePath( outPath );
+						@Override
+						public TileInfo call( final TileInfo tile ) throws Exception
+						{
+							final String outPath = outFolder+"/MID_"+Paths.get(tile.getFilePath()).getFileName().toString();
+							extractMiddleSlice( tile, outPath );
 
-						return middleSliceTile;
-					}
-				});
+							final TileInfo middleSliceTile = new TileInfo(tile.numDimensions() - 1);
+							middleSliceTile.setIndex( tile.getIndex() );
+							middleSliceTile.setType( tile.getType() );
+							middleSliceTile.setPosition( new double[] { tile.getPosition(0), tile.getPosition(1) } );
+							middleSliceTile.setSize( new long[] { tile.getSize(0), tile.getSize(1) } );
+							middleSliceTile.setFilePath( outPath );
 
-		final TileInfo[] middleSliceTiles = task.collect().toArray( new TileInfo[0] );
+							return middleSliceTile;
+						}
+					});
+
+			middleSliceTiles = task.collect().toArray( new TileInfo[0] );
+		}
+
 		TileInfoJSONProvider.saveTilesConfiguration( middleSliceTiles, DataProviderFactory.createFSDataProvider().getJsonWriter( URI.create( Utils.addFilenameSuffix( input, "_middle" ) ) ) );
-
 		System.out.println("Done");
 	}
 
-	private < T extends RealType< T > & NativeType< T > > void extractMiddleSlice( final TileInfo tile, final String outPath )
+	private static < T extends RealType< T > & NativeType< T > > void extractMiddleSlice( final TileInfo tile, final String outPath )
 	{
 		final ImagePlus imp = IJ.openImage(tile.getFilePath());
 		Utils.workaroundImagePlusNSlices(imp);
