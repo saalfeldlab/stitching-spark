0a6d0935181143dc807da7d28d69cb0b7a7a516a
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-02-16 19:12:49 -0500

    skip images that do not exist when extracting middle slice

--- src/main/java/org/janelia/stitching/analysis/ExtractMiddleSlice.java
+++ src/main/java/org/janelia/stitching/analysis/ExtractMiddleSlice.java
@@ -2,15 +2,17 @@ package org.janelia.stitching.analysis;
 
 import java.io.File;
 import java.net.URI;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.janelia.dataaccess.DataProviderFactory;
-import org.janelia.stitching.ImageType;
 import org.janelia.stitching.TileInfo;
 import org.janelia.stitching.TileInfoJSONProvider;
 import org.janelia.stitching.Utils;
@@ -26,6 +28,7 @@ import net.imglib2.img.imageplus.ImagePlusImgFactory;
 import net.imglib2.img.imageplus.ImagePlusImgs;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
+import net.imglib2.util.Util;
 import net.imglib2.view.Views;
 
 public class ExtractMiddleSlice
@@ -37,7 +40,7 @@ public class ExtractMiddleSlice
 		new File( outFolder ).mkdirs();
 
 		final TileInfo[] tiles = TileInfoJSONProvider.loadTilesConfiguration( DataProviderFactory.createFSDataProvider().getJsonReader( URI.create( input ) ) );
-		final TileInfo[] middleSliceTiles;
+		final List< TileInfo > middleSliceTiles;
 
 		try ( final JavaSparkContext sparkContext = new JavaSparkContext( new SparkConf().setAppName( "ExtractMiddleSlice" ) ) )
 		{
@@ -50,6 +53,9 @@ public class ExtractMiddleSlice
 						@Override
 						public TileInfo call( final TileInfo tile ) throws Exception
 						{
+							if ( !Files.exists( Paths.get( tile.getFilePath() ) ) )
+								return null;
+
 							final String outPath = outFolder+"/MID_"+Paths.get(tile.getFilePath()).getFileName().toString();
 							extractMiddleSlice( tile, outPath );
 
@@ -64,10 +70,14 @@ public class ExtractMiddleSlice
 						}
 					});
 
-			middleSliceTiles = task.collect().toArray( new TileInfo[0] );
+			middleSliceTiles = task.collect();
+			middleSliceTiles.removeAll( Collections.singleton( null ) );
 		}
 
-		TileInfoJSONProvider.saveTilesConfiguration( middleSliceTiles, DataProviderFactory.createFSDataProvider().getJsonWriter( URI.create( Utils.addFilenameSuffix( input, "_middle" ) ) ) );
+		TileInfoJSONProvider.saveTilesConfiguration(
+				middleSliceTiles.toArray( new TileInfo[ 0 ] ),
+				DataProviderFactory.createFSDataProvider().getJsonWriter( URI.create( Utils.addFilenameSuffix( input, "_middle" ) ) )
+			);
 		System.out.println("Done");
 	}
 
@@ -76,13 +86,11 @@ public class ExtractMiddleSlice
 		final ImagePlus imp = IJ.openImage(tile.getFilePath());
 		Utils.workaroundImagePlusNSlices(imp);
 
-		final T type = (T) ImageType.valueOf(imp.getType()).getType();
-
 		final long[] outputDim = tile.getSize().clone();
 		outputDim[ 2 ] = 1;
 
 		final Img< T > img = ImagePlusImgs.from(imp);
-		final Img< T > out = new ImagePlusImgFactory< T >().create( outputDim, type.createVariable() );
+		final Img< T > out = new ImagePlusImgFactory< T >().create( outputDim, Util.getTypeFromInterval( img ) );
 
 		final int slice = (int)tile.getSize(2) / 2;
 		final Interval interval = new FinalInterval( new long[] { 0, 0, slice }, new long[] { tile.getSize(0)-1, tile.getSize(1)-1, slice } );
