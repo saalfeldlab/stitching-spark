82651ed2b1e42d49dbc12c322c7765df7b0fe7f7
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-03-17 00:48:18 -0400

    fix calculation of slabMin when only tile subset is used

--- src/main/java/org/janelia/stitching/C1WarpedMetadata.java
+++ src/main/java/org/janelia/stitching/C1WarpedMetadata.java
@@ -8,6 +8,7 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -19,6 +20,8 @@ import org.janelia.dataaccess.DataProviderFactory;
 import bdv.img.TpsTransformWrapper;
 import bigwarp.Landmarks2Transform;
 import jitk.spline.ThinPlateR2LogRSplineKernelTransform;
+import net.imglib2.util.Intervals;
+import net.imglib2.util.Util;
 
 public class C1WarpedMetadata
 {
@@ -54,12 +57,43 @@ public class C1WarpedMetadata
 	private static final String BASE_PATH = "/nrs/saalfeld/igor/illumination-correction/Sample1_C1/stitching/restitching-affine";
 	private static final double[] PIXEL_RESOLUTION = new double[] { 0.097, 0.097, 0.18 };
 
+	public static TileSlabMapping getTileSlabMapping() throws IOException
+	{
+		// fill tile metadata
+		final List< Map< String, TileInfo[] > > slabsTilesChannels = new ArrayList<>();
+		for ( int ch = 0; ch < NUM_CHANNELS; ++ch )
+		{
+			final Map< String, TileInfo[] > slabsTiles = new HashMap<>();
+			for ( final String slab : getSlabs() )
+				slabsTiles.put( slab, getSlabTiles( slab, ch ) );
+			slabsTilesChannels.add( slabsTiles );
+		}
+
+		final int[] tileCount = new int[ slabsTilesChannels.size() ];
+		for ( int ch = 0; ch < tileCount.length; ++ch )
+			for ( final TileInfo[] slabTiles : slabsTilesChannels.get( ch ).values() )
+				tileCount[ ch ] += slabTiles.length;
+		for ( int ch = 0; ch < tileCount.length; ++ch )
+			System.out.println( "Total tiles in ch" + ch + ": " + tileCount[ ch ] );
+
+		// fill slab metadata
+		final Map< String, double[] > slabsMin = new HashMap<>();
+		final Map< String, TpsTransformWrapper > slabsTransforms = new HashMap<>();
+		for ( final String slab : getSlabs() )
+		{
+			slabsMin.put( slab, getSlabMin( slab ) );
+			slabsTransforms.put( slab, getSlabTransform( slab ) );
+		}
+
+		return new TileSlabMapping( slabsTilesChannels, slabsMin, slabsTransforms );
+	}
+
 	public static Set< String > getSlabs()
 	{
 		return Collections.unmodifiableSet( LANDMARKS.keySet() );
 	}
 
-	public static TpsTransformWrapper getTransform( final String slab ) throws IOException
+	public static TpsTransformWrapper getSlabTransform( final String slab ) throws IOException
 	{
 		if ( LANDMARKS.get( slab ) == null )
 			return new TpsTransformWrapper( NUM_DIMENSIONS );
@@ -70,7 +104,34 @@ public class C1WarpedMetadata
 		return new TpsTransformWrapper( transformRaw.getNumDims(), transformRaw );
 	}
 
+	public static double[] getSlabMin( final String slab ) throws IOException
+	{
+		// make sure that slabMin is the same for all channels
+		double[] slabMin = null;
+		for ( int ch = 0; ch < NUM_CHANNELS; ++ch )
+		{
+			final TileInfo[] chSlabTiles = getSlabTiles( slab, ch, false );
+			final double[] chSlabMin = Intervals.minAsDoubleArray( TileOperations.getRealCollectionBoundaries( chSlabTiles ) );
+			if ( slabMin == null )
+			{
+				slabMin = chSlabMin.clone();
+			}
+			else
+			{
+				for ( int d = 0; d < slabMin.length; ++d )
+					if ( !Util.isApproxEqual( slabMin[ d ], chSlabMin[ d ], 1e-10 ) )
+						throw new RuntimeException( "slabMin is different for ch0/ch1" );
+			}
+		}
+		return slabMin;
+	}
+
 	public static TileInfo[] getSlabTiles( final String slab, final int channel ) throws IOException
+	{
+		return getSlabTiles( slab, channel, true );
+	}
+
+	private static TileInfo[] getSlabTiles( final String slab, final int channel, final boolean removeMissing ) throws IOException
 	{
 		final Path slabPath = Paths.get( SLABS_DIR, slab, "ch" + channel + "-" + slab + "-final.json" );
 		final List< TileInfo > slabTiles = new ArrayList<>(
@@ -89,6 +150,8 @@ public class C1WarpedMetadata
 
 		// hack to remove a non-existing tile 18808 in ch1
 		if ( channel == 1 )
+		// new mode: if requested, remove missing tiles in both channels
+		if ( removeMissing )
 		{
 			for ( final Iterator< TileInfo > it = slabTiles.iterator(); it.hasNext(); )
 			{
