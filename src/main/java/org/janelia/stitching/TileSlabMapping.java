07035e58e69ff016d19788260a10f938cc372621
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-03-19 10:16:11 -0400

    validate slab min

--- src/main/java/org/janelia/stitching/TileSlabMapping.java
+++ src/main/java/org/janelia/stitching/TileSlabMapping.java
@@ -1,31 +1,18 @@
 package org.janelia.stitching;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import bdv.img.TpsTransformWrapper;
+import net.imglib2.util.Util;
 
 public class TileSlabMapping implements Serializable
 {
 	private static final long serialVersionUID = 5234507500482092480L;
-
-	/*public static TileSlabMapping instance;
-
-	public static void createInstance(
-			final List< Map< String, TileInfo[] > > slabsTilesChannels,
-			final Map< String, double[] > slabsMin,
-			final Map< String, TpsTransformWrapper > slabsTransforms )
-	{
-		instance = new TileSlabMapping( slabsTilesChannels, slabsMin, slabsTransforms );
-	}
-
-	public static TileSlabMapping getInstance()
-	{
-		return instance;
-	}*/
 
 	private final Map< String, double[] > slabsMin;
 	private final Map< String, TpsTransformWrapper > slabsTransforms;
@@ -53,7 +40,11 @@ public class TileSlabMapping implements Serializable
 
 	public double[] getSlabMin( final String slab )
 	{
-		return slabsMin.get( slab );
+		// for C1, all slabMin are zeroes
+		final double[] slabMin = slabsMin.get( slab );
+		if ( !Util.isApproxEqual( Arrays.stream( slabMin ).min().getAsDouble(), 0, 1e-10 ) || !Util.isApproxEqual( Arrays.stream( slabMin ).max().getAsDouble(), 0, 1e-10 ) )
+			throw new RuntimeException( "slabMin should be 0" );
+		return slabMin;
 	}
 
 	public TpsTransformWrapper getSlabTransform( final String slab )
