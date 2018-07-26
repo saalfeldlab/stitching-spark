48d4783f88d009577bc71a72c3cd3535aef65852
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-07-26 13:45:15 -0400

    add retile local & cluster startup scripts

---
+++ startup-scripts/spark-local/retile.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+
+sys.dont_write_bytecode = True
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from jar_path_util import get_jar_path
+bin_path = get_jar_path()
+
+subprocess.call(['java', '-Dspark.master=local[*]', '-cp', bin_path, 'org.janelia.stitching.ResaveAsSmallerTilesSpark'] + sys.argv[1:])
