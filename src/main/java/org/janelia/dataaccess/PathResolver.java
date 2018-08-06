f1ba52f091f87a06a58139255b395b60a524ba59
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-08-06 12:09:56 -0400

    make PathResolver work properly with filesystem paths containing spaces

--- src/main/java/org/janelia/dataaccess/PathResolver.java
+++ src/main/java/org/janelia/dataaccess/PathResolver.java
@@ -6,20 +6,17 @@ import java.nio.file.Paths;
 public class PathResolver
 {
 	/**
-	 * Combines base and relative paths.
-	 * Supports links and local filesystem paths.
+	 * Combines base and relative paths. Supports both links and filesystem paths.
 	 *
-	 * @param base
-	 * @param rel
+	 * @param basePath
+	 * @param relativePaths
 	 * @return
 	 */
 	public static String get( final String basePath, final String... relativePaths )
 	{
-		// if a given path is a link, need to use URI functionality because Paths.get() in this case swallows the scheme
-		final URI baseUri = URI.create( basePath );
-		if ( baseUri.getScheme() != null )
+		if ( CloudURI.isCloudURI( basePath ) )
 		{
-			URI combinedUri = baseUri;
+			URI combinedUri = URI.create( basePath );
 			for ( final String relativePath : relativePaths )
 			{
 				// base URI has to end with a slash, otherwise relative path replaces the last part of the base URI
@@ -38,10 +35,11 @@ public class PathResolver
 
 	public static String getParent( final String path )
 	{
-		// if a given path is a link, need to use URI functionality because Paths.get() in this case swallows the scheme
-		final URI uri = URI.create( path );
-		if ( uri.getScheme() != null )
+		if ( CloudURI.isCloudURI( path ) )
 		{
+			// if a given path is a link, need to use URI functionality because Paths.get() in this case swallows the scheme
+			final URI uri = URI.create( path );
+
 			// based on https://stackoverflow.com/a/10159309
 			final URI parentUri = uri.getPath().endsWith( "/" ) ? uri.resolve( ".." ) : uri.resolve( "." );
 			return parentUri.toString();
