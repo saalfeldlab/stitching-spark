88f6afebdef0eac41a267d550f4d7f1a52ac077d
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-09-25 17:18:10 -0400

    modify pairwise diff script to group by subtiles and print verbose info

--- src/main/java/org/janelia/stitching/analysis/PrintDiffBetweenPairwiseShifts.java
+++ src/main/java/org/janelia/stitching/analysis/PrintDiffBetweenPairwiseShifts.java
@@ -1,15 +1,15 @@
 package org.janelia.stitching.analysis;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 import java.util.TreeMap;
 
 import org.janelia.dataaccess.DataProvider;
 import org.janelia.dataaccess.DataProviderFactory;
 import org.janelia.stitching.SerializablePairWiseStitchingResult;
-import org.janelia.stitching.TileInfo;
+import org.janelia.stitching.SubTile;
 import org.janelia.stitching.TileInfoJSONProvider;
 
 public class PrintDiffBetweenPairwiseShifts
@@ -22,40 +22,53 @@ public class PrintDiffBetweenPairwiseShifts
 		for ( int i = 0; i < 2; ++i )
 			pairwiseShifts[ i ] = TileInfoJSONProvider.loadPairwiseShifts( dataProvider.getJsonReader( URI.create( pairwisePath[ i ] ) ) );
 
-		final Map< Integer, Map< Integer, Boolean > > fullTilePairwiseMap = new TreeMap<>();
+		for ( final List< SerializablePairWiseStitchingResult > pairwiseShiftList : pairwiseShifts )
+			for ( final SerializablePairWiseStitchingResult pairwiseShift : pairwiseShiftList )
+				if ( !pairwiseShift.getIsValidOverlap() )
+					throw new RuntimeException( "contains invalid elements" );
+
+		System.out.println( "Size of A: " + pairwiseShifts[ 0 ].size() );
+		System.out.println( "Size of B: " + pairwiseShifts[ 1 ].size() );
+		System.out.println();
+
+		final Map< Integer, Map< Integer, SerializablePairWiseStitchingResult > > subTilePairwiseMap = new TreeMap<>();
 
 		for ( final SerializablePairWiseStitchingResult shift : pairwiseShifts[ 0 ] )
 		{
-			final TileInfo[] tilePair = shift.getSubTilePair().getFullTilePair().toArray();
-			final int ind1 = Math.min( tilePair[ 0 ].getIndex(), tilePair[ 1 ].getIndex() );
-			final int ind2 = Math.max( tilePair[ 0 ].getIndex(), tilePair[ 1 ].getIndex() );
+			final SubTile[] subTilePair = shift.getSubTilePair().toArray();
+			final int ind1 = Math.min( subTilePair[ 0 ].getIndex(), subTilePair[ 1 ].getIndex() );
+			final int ind2 = Math.max( subTilePair[ 0 ].getIndex(), subTilePair[ 1 ].getIndex() );
 
-			if ( !fullTilePairwiseMap.containsKey( ind1 ) )
-				fullTilePairwiseMap.put( ind1, new TreeMap<>() );
-			fullTilePairwiseMap.get( ind1 ).put( ind2, false );
+			if ( !subTilePairwiseMap.containsKey( ind1 ) )
+				subTilePairwiseMap.put( ind1, new TreeMap<>() );
+			subTilePairwiseMap.get( ind1 ).put( ind2, shift );
 		}
 
-		final Map< Integer, Integer > missingInA = new TreeMap<>();
+		final List< SerializablePairWiseStitchingResult > missingInA = new ArrayList<>();
 		for ( final SerializablePairWiseStitchingResult shift : pairwiseShifts[ 1 ] )
 		{
-			final TileInfo[] tilePair = shift.getSubTilePair().getFullTilePair().toArray();
-			final int ind1 = Math.min( tilePair[ 0 ].getIndex(), tilePair[ 1 ].getIndex() );
-			final int ind2 = Math.max( tilePair[ 0 ].getIndex(), tilePair[ 1 ].getIndex() );
+			final SubTile[] subTilePair = shift.getSubTilePair().toArray();
+			final int ind1 = Math.min( subTilePair[ 0 ].getIndex(), subTilePair[ 1 ].getIndex() );
+			final int ind2 = Math.max( subTilePair[ 0 ].getIndex(), subTilePair[ 1 ].getIndex() );
 
-			if ( !fullTilePairwiseMap.containsKey( ind1 ) || !fullTilePairwiseMap.get( ind1 ).containsKey( ind2 ) )
-				missingInA.put( ind1, ind2 );
+			if ( !subTilePairwiseMap.containsKey( ind1 ) || !subTilePairwiseMap.get( ind1 ).containsKey( ind2 ) )
+			{
+				missingInA.add( shift );
+			}
 			else
-				fullTilePairwiseMap.get( ind1 ).put( ind2, true );
+			{
+				subTilePairwiseMap.get( ind1 ).get( ind2 ).setIsValidOverlap( false );
+			}
 		}
 		System.out.println( "Missing in A:" );
-		for ( final Entry< Integer, Integer > entry : missingInA.entrySet() )
-			System.out.println( " (" + entry.getKey() + "," + entry.getValue() + ")" );
+		for ( final SerializablePairWiseStitchingResult pairwiseShift : missingInA )
+			System.out.println( "  " + pairwiseShift.getSubTilePair() );
 		System.out.println();
 
 		System.out.println( "Missing in B:" );
-		for ( final Entry< Integer, Map< Integer, Boolean > > entry : fullTilePairwiseMap.entrySet() )
-			for ( final Entry< Integer, Boolean > entryForKey : entry.getValue().entrySet() )
-				if ( !entryForKey.getValue() )
-					System.out.println( " (" + entry.getKey() + "," + entryForKey.getKey() + ")" );
+		for ( final Map< Integer, SerializablePairWiseStitchingResult > values : subTilePairwiseMap.values() )
+			for ( final SerializablePairWiseStitchingResult pairwiseShift : values.values() )
+				if ( pairwiseShift.getIsValidOverlap() )
+					System.out.println( "  " + pairwiseShift.getSubTilePair() );
 	}
 }
