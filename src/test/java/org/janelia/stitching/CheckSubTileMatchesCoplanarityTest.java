dd0dbea9ee18b3ca2b67a3c71c20291c01f7a239
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-10-09 10:43:27 -0400

    add test for configuration coplanarity check

---
+++ src/test/java/org/janelia/stitching/CheckSubTileMatchesCoplanarityTest.java
@@ -0,0 +1,59 @@
+package org.janelia.stitching;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeMap;
+
+import org.janelia.util.ComparableTuple;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CheckSubTileMatchesCoplanarityTest
+{
+	@Test
+	public void test()
+	{
+		final TileInfo tile = new TileInfo( 3 );
+		tile.setSize( new long[] { 100, 200, 300 } );
+
+		final List< SubTile > subTiles = SubTileOperations.subdivideTiles( new TileInfo[] { tile }, new int[] { 2, 2, 2 } );
+		Assert.assertEquals( 8, subTiles.size() );
+
+		final int numConfigs = 1 << subTiles.size();
+		Assert.assertEquals( 256, numConfigs );
+
+		int numConfigsWithFewerThan4Points = 0;
+		int numCoplanarConfigsWithFewerThan4Points = 0, numCoplanarConfigsWith4Points = 0, numCoplanarConfigsWithMoreThan4Points = 0;
+		for ( int mask = 0; mask < numConfigs; ++mask )
+		{
+			final List< SubTile > subTilesConfig = new ArrayList<>();
+			for ( int i = 0; i < subTiles.size(); ++i )
+				if ( ( mask & ( 1 << i ) ) > 0 )
+					subTilesConfig.add( subTiles.get( i ) );
+
+			final List< SubTile > subTilesConfigWithDuplicates = new ArrayList<>();
+			for ( int k = 0; k < 3; ++k )
+				subTilesConfigWithDuplicates.addAll( subTilesConfig );
+
+			final TreeMap< ComparableTuple< Long >, Integer > groupedSubTiles = CheckSubTileMatchesCoplanarity.groupSubTilesByTheirLocalPosition( subTilesConfigWithDuplicates );
+			Assert.assertEquals( subTilesConfig.size(), groupedSubTiles.size() );
+
+			if ( subTilesConfig.size() < 4 )
+				++numConfigsWithFewerThan4Points;
+
+			if ( CheckSubTileMatchesCoplanarity.isCoplanar( groupedSubTiles ) )
+			{
+				if ( groupedSubTiles.size() < 4 )
+					++numCoplanarConfigsWithFewerThan4Points;
+				else if ( groupedSubTiles.size() == 4 )
+					++numCoplanarConfigsWith4Points;
+				else if ( groupedSubTiles.size() > 4 )
+					++numCoplanarConfigsWithMoreThan4Points;
+			}
+		}
+
+		Assert.assertEquals( numConfigsWithFewerThan4Points, numCoplanarConfigsWithFewerThan4Points );
+		Assert.assertEquals( 12, numCoplanarConfigsWith4Points ); // 6 orthogonal configs + 6 tilted configs
+		Assert.assertEquals( 0, numCoplanarConfigsWithMoreThan4Points );
+	}
+}
