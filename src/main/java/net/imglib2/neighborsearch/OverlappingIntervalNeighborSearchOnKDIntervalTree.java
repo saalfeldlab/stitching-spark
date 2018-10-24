dcd5ecebc14eb5bef0f5496ea3963d2531c50024
Igor Pisarev <pisarevi@janelia.hhmi.org>
2018-10-24 14:19:40 -0400

    fix overlapping interval search strategy for fully containing intervals
    For intervals in the tree that fully contain the queried interval, no corner points can be found inside the queried intervals, so such intervals were not included in the resulting set. The fix is to find all corner points < queried min and all corner points > queried max, then intersect these two sets

--- src/main/java/net/imglib2/neighborsearch/OverlappingIntervalNeighborSearchOnKDIntervalTree.java
+++ src/main/java/net/imglib2/neighborsearch/OverlappingIntervalNeighborSearchOnKDIntervalTree.java
@@ -41,7 +41,9 @@ import java.util.Map;
 
 import net.imglib2.KDIntervalTree;
 import net.imglib2.KDIntervalTree.ValueWithId;
+import net.imglib2.KDTreeNode;
 import net.imglib2.RealInterval;
+import net.imglib2.RealLocalizable;
 
 public class OverlappingIntervalNeighborSearchOnKDIntervalTree< T > implements IntervalNeighborSearch< T >
 {
@@ -55,15 +57,95 @@ public class OverlappingIntervalNeighborSearchOnKDIntervalTree< T > implements I
 	@Override
 	public List< T > search( final RealInterval interval )
 	{
+		// find corners contained inside the queried interval
 		final List< ValueWithId< T > > found = intervalNeighborSearchOnCornerPointsKDTree.search( interval );
 		final Map< Integer, T > foundUnique = new LinkedHashMap<>();
 		found.forEach( valueWithId -> foundUnique.put( valueWithId.id, valueWithId.value ) );
-		return new ArrayList<>( foundUnique.values() );
+
+		// find intervals that fully contain the queried interval (and thus were not included in the above set because all their corners are outside the queried interval)
+		final List< T > foundFullyContaining = findFullyContaining( interval );
+
+		final List< T > result = new ArrayList<>();
+		result.addAll( foundUnique.values() );
+		result.addAll( foundFullyContaining );
+
+		return result;
 	}
 
 	@Override
 	public int numDimensions()
 	{
 		return intervalNeighborSearchOnCornerPointsKDTree.numDimensions();
+	}
+
+	protected List< T > findFullyContaining( final RealInterval interval )
+	{
+		// find points with coordinates smaller than min of the queried interval in all dimensions
+		final List< ValueWithId< T > > foundLeft = new ArrayList<>();
+		findLeft( intervalNeighborSearchOnCornerPointsKDTree.tree.getRoot(), interval, foundLeft );
+		final Map< Integer, T > foundLeftUnique = new LinkedHashMap<>();
+		foundLeft.forEach( valueWithId -> foundLeftUnique.put( valueWithId.id, valueWithId.value ) );
+
+		// find points with coordinates larger than max of the queried interval in all dimensions
+		final List< ValueWithId< T > > foundRight = new ArrayList<>();
+		findRight( intervalNeighborSearchOnCornerPointsKDTree.tree.getRoot(), interval, foundRight );
+		final Map< Integer, T > foundRightUnique = new LinkedHashMap<>();
+		foundRight.forEach( valueWithId -> foundRightUnique.put( valueWithId.id, valueWithId.value ) );
+
+		// intervals fully contain the queried interval when there is one point < min (top-left corner) and one point > max (bottom-right corner)
+		final Map< Integer, T > foundUnique = new LinkedHashMap<>( foundLeftUnique );
+		foundUnique.keySet().retainAll( foundRightUnique.keySet() );
+
+		return new ArrayList<>( foundUnique.values() );
+	}
+
+	protected static < T > void findLeft( final KDTreeNode< ValueWithId< T > > current, final RealInterval interval, final List< ValueWithId< T > > foundLeft )
+	{
+		if ( testLeft( current, interval ) )
+			foundLeft.add( current.get() );
+
+		if ( current.left != null )
+			findLeft( current.left, interval, foundLeft );
+
+		if ( current.right != null && !testRight( current, interval, current.getSplitDimension() ) )
+			findLeft( current.right, interval, foundLeft );
+	}
+
+	private static boolean testLeft( final RealLocalizable pos, final RealInterval interval )
+	{
+		for ( int d = 0; d < pos.numDimensions(); ++d )
+			if ( !testLeft( pos, interval, d ) )
+				return false;
+		return true;
+	}
+
+	private static boolean testLeft( final RealLocalizable pos, final RealInterval interval, final int d )
+	{
+		return pos.getDoublePosition( d ) < interval.realMin( d );
+	}
+
+	protected static < T > void findRight( final KDTreeNode< ValueWithId< T > > current, final RealInterval interval, final List< ValueWithId< T > > foundRight )
+	{
+		if ( testRight( current, interval ) )
+			foundRight.add( current.get() );
+
+		if ( current.left != null && !testLeft( current, interval, current.getSplitDimension() ) )
+			findRight( current.left, interval, foundRight );
+
+		if ( current.right != null )
+			findRight( current.right, interval, foundRight );
+	}
+
+	private static boolean testRight( final RealLocalizable pos, final RealInterval interval )
+	{
+		for ( int d = 0; d < pos.numDimensions(); ++d )
+			if ( !testRight( pos, interval, d ) )
+				return false;
+		return true;
+	}
+
+	private static boolean testRight( final RealLocalizable pos, final RealInterval interval, final int d )
+	{
+		return pos.getDoublePosition( d ) > interval.realMax( d );
 	}
 }
