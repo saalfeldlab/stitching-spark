d8e4352ddccc402aaaca83e1864ead3f36fbcb6f
Igor Pisarev <pisarevi@janelia.hhmi.org>
2019-04-24 16:48:23 -0400

    set current working directory properly when compiling matlab decon code

--- matlab/compile.py
+++ matlab/compile.py
@@ -5,7 +5,9 @@ import subprocess
 
 matlab_root_path = '/misc/local/matlab-2018b'
 matlab_compiler = os.path.join(matlab_root_path, 'bin', 'mcc')
-matlab_program_filepath = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'matlab_decon.m')
+
+working_dirpath = os.path.dirname(os.path.realpath(__file__))
+matlab_program_filepath = os.path.join(working_dirpath, 'matlab_decon.m')
 
 if __name__ == '__main__':
-	subprocess.call([matlab_compiler, '-m', '-R', '-nojvm', '-v', matlab_program_filepath])
+	subprocess.call([matlab_compiler, '-m', '-R', '-nojvm', '-v', matlab_program_filepath], cwd=working_dirpath)
