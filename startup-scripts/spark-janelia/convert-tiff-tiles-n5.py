817a87dbf5fa569a32c111d3a97e5876006fd78d
Igor Pisarev <pisarevi@janelia.hhmi.org>
2019-11-07 15:04:55 -0500

    update startup scripts for tiff->n5 and czi->n5 conversion

---
+++ startup-scripts/spark-janelia/convert-tiff-tiles-n5.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+from submit import submit
+submit('org.janelia.stitching.ConvertTIFFTilesToN5Spark')
--- startup-scripts/spark-janelia/convert-tiles-n5.py
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env python
-
-from submit import submit
-submit('org.janelia.stitching.ConvertTilesToN5Spark')
