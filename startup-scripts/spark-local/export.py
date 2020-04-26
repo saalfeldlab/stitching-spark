82cc45f0467860c9a6b3dc46af9f7afa01a62024
Igor Pisarev <pisarevi@janelia.hhmi.org>
2020-04-25 20:45:39 -0400

    increase processing block in local startup scripts for n5 export to make it faster
    on a small number of CPU cores it's faster to process larger blocks at once to minimize the number of reads of each tile image

--- startup-scripts/spark-local/export.py
+++ startup-scripts/spark-local/export.py
@@ -9,4 +9,4 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from jar_path_util import get_jar_path
 bin_path = get_jar_path()
 
-subprocess.call(['java', '-Dspark.master=local[*]', '-cp', bin_path, 'org.janelia.stitching.StitchingSpark', '--fuse'] + sys.argv[1:])
+subprocess.call(['java', '-Dspark.master=local[*]', '-cp', bin_path, 'org.janelia.stitching.StitchingSpark', '--fuse', '-f', '512'] + sys.argv[1:])
