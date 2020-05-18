000144b5083fcb7f8c83d0cb44f860035231af2b
Igor Pisarev <igorpisarev@users.noreply.github.com>
2020-05-18 18:58:15 -0400

    add spark.master local configuration to z1 metadata parsing as well

--- startup-scripts/spark-local/parse-zeiss-z1-metadata.py
+++ startup-scripts/spark-local/parse-zeiss-z1-metadata.py
@@ -9,4 +9,4 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from jar_path_util import get_jar_path
 bin_path = get_jar_path()
 
-subprocess.call(['java', '-cp', bin_path, 'org.janelia.stitching.ParseCZITilesMetadata'] + sys.argv[1:])
+subprocess.call(['java', '-Dspark.master=local[1]', '-cp', bin_path, 'org.janelia.stitching.ParseCZITilesMetadata'] + sys.argv[1:])
